54f036ce134423abaed516431e481e2ed33a367b
MrUtcka <mini_utcka2007@mail.ru>
2025-12-02 22:56:53 +0700

    UPDATE!!! Console input for data: ------------------------------- bind for open console: f1 day to enter to server: monday time to enter to server: 00:00 server ip: ip ------------------------------- Added quit() after print statements to end script after entering server

--- rust.py
+++ rust.py
@@ -2,19 +2,47 @@ import schedule as sch
 from time import sleep
 import mouse, keyboard
 
+#binds
+keyForOpenConsole = 'f1'
+
+#connect data
+serverIP = 'ip' #without connect
+dayToEnter = 'monday' #name
+timeToEnter = '00:00' #only hh:mm
+
+
 def enter():
-    keyboard.send('f1')
+    global keyForOpenConsole, serverIP
+    
+    keyboard.send(keyForOpenConsole)
     sleep(0.3)
-    keyboard.write('connect 19.magicrust.gg')
+    keyboard.write('connect ' + serverIP)
     sleep(0.3)
     keyboard.send('enter')
     sleep(0.3)
-    keyboard.send('f1')
+    keyboard.send(keyForOpenConsole)
     sleep(0.2)
+    
+    print('\n' + 'entered to server ' + serverIP)
+    quit()
+
+
+def inputData():
+    global keyForOpenConsole, serverIP, dayToEnter, timeToEnter
+    
+    keyForOpenConsole = input('enter your bind for open console *f1*: ').lower()
+    dayToEnter = input('enter day to enter to server *monday*: ').lower()
+    timeToEnter = input('enter time to enter to server *00:00*: ').lower()
+    serverIP = input('enter server ip *ip*: ').lower()
 
     
 def main():
-    sch.every().friday.at('13:59:58').do(enter)
+    global dayToEnter, timeToEnter
+    
+    inputData()
+    print('\n' + 'auto enter scheduled for ' + dayToEnter + ' at ' + timeToEnter + ' to server ' + serverIP)
+    
+    getattr(sch.every(), dayToEnter).at(timeToEnter + ':58').do(enter)
 
     while True:
         sch.run_pending()
